5b477004059f67f0fdacdce4f027628a3c42a90e
Tap-Kim <rlaxogus0517@gmail.com>
2021-11-24 22:26:57 +0900

    TypeScript 적용, 부트스트랩 적용

---
+++ src/Component/Main/Main.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Main = () => {
+    return (
+        <div className="main">
+            <button type="button" className="btn btn-danger">danger</button>
+        </div>
+    );
+};
+
+export default Main;
